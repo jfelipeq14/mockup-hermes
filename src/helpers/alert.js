14802c7f6f8d5a8fab742758dccbbad9fb546c76
jfelipe14 <juan_fquintero@soy.sena.edu.co>
2024-03-01 12:17:59 -0500

    add alert static

--- src/helpers/alert.js
+++ src/helpers/alert.js
@@ -1,3 +1,23 @@
-(() => {
-  console.log('Funciona')
-})()
+// const type = ['success', 'danger', 'info']
+
+const alertPlaceholder = document.getElementById('alerts')
+const appendAlert = (message, type) => {
+  const wrapper = document.createElement('article')
+  wrapper.innerHTML = [
+          `<div class="alert alert-${type} alert-dismissible m-5 fixed-top" role="alert">`,
+          `   <span>${message}</span>`,
+          '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+          '</div>'
+  ].join('')
+
+  alertPlaceholder.append(wrapper)
+}
+
+const alertTrigger = document.getElementById('btnAlerts')
+if (alertTrigger) {
+  alertTrigger.addEventListener('click', (e) => {
+    e.preventDefault()
+    console.log(e.target)
+    appendAlert('Mensaje de la alerta', 'success')
+  })
+}
