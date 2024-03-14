f4281ba331548cfe594283e181c74a5b0b8da0b7
jfelipe14 <juan_fquintero@soy.sena.edu.co>
2024-03-14 18:03:51 -0500

    add new styles and new content

---
+++ src/components/customers/services/services.js
@@ -0,0 +1,10 @@
+export const serviceContent = [
+  '<section class="container rounded" id="services">',
+  '  <header class="col-sm-12 text-center">',
+  '    <p class="fs-3">Encuentra nuestros mejores servicios</p>',
+  '    <h2 class="text-capitalize fs-2">SERVICIOS</h2>',
+  '  </header>',
+  '  <article class="row justify-content-center align-items-center p-2" id="cards-services">',
+  '  </article>',
+  '</section>'
+].join('')
--- src/service/main.js
+++ src/service/main.js
@@ -1,30 +1,54 @@
-import { pageOfertas } from '../screens/ofertas/ofertas.js'
-import { pageBooking } from '../screens/reserva/reserva.js'
-import { pageCredits } from '../screens/credits/credits.js'
-import { articleCarrusel } from '../screens/carrousel/carrusel.js'
+import { serviceContent } from '../components/customers/services/services.js'
+import { aboutContent } from '../components/layout/index.js'
 
-const content = document.getElementById('content')
-const btnOfertas = document.getElementById('btnOfertas')
-const btnReservas = document.getElementById('btnReservas')
-const btnCreditos = document.getElementById('btnCreditos')
-const reloadPage = document.getElementById('reloadPage')
+const services = [
+  {
+    nombre: 'baru',
+    descripcion: 'playa de x cosa',
+    precio: 2000000,
+    url: 'img'
+  },
+  {
+    nombre: 'rincon',
+    descripcion: 'playa de y cosa',
+    precio: 1200000,
+    url: 'img'
+  },
+  {
+    nombre: 'coveñas',
+    descripcion: 'playa de z cosa',
+    precio: 800000,
+    url: 'img'
+  }
+]
 
-btnOfertas.addEventListener('click', () => {
-  content.innerHTML = pageOfertas
-})
+const $content = document.getElementById('content')
 
-btnReservas.addEventListener('click', () => {
-  content.innerHTML = pageBooking
-})
+if ($content) {
+  $content.innerHTML += serviceContent
+  $content.innerHTML += aboutContent
 
-btnCreditos.addEventListener('click', () => {
-  content.innerHTML = pageCredits
-})
+  const $cardsServices = document.getElementById('cards-services')
+  if ($cardsServices) {
+    services.forEach((service) => {
+      $cardsServices.innerHTML += `
+      <article class="card col-sm-7 col-md-3 m-2 shadow">
+      <header class="card-header">
+        <h3 class="card-title">${service.nombre}</h3>
+      </header>
+      <article class="card-body">
+        <picture>
+          <img src="${service.url}" alt="${service.descripcion}" class="card-img">
+        </picture>
+        <p class="card-title">${service.descripcion}</p>
+        <button class="btn btn-primary btn-sm text-center">RESERVAR AHORA</button>
+      </article>
+      <footer class="card-footer">${service.precio}</footer>
+    </article>
+    `
+    })
+  }
 
-window.addEventListener('load', () => {
-  content.innerHTML = articleCarrusel
-})
-
-reloadPage.addEventListener('click', () => {
-  location.reload()
-})
+  // $btnAbout.addEventListener('click', () => {
+  // })
+}
